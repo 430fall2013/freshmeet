7eca3d62e5af35f8583fda15f8f7102543e2138f
Tim Whitaker <tmwhtkr@gmail.com>
2013-10-15 18:11:12 -0700

    active nav class is now better. code in layout.js

--- client/managers/layout.js
+++ client/managers/layout.js
@@ -1,9 +1,18 @@
 Template.nav.rendered = function() {
-	// var path = Router._current.path;
-	// if (path == '/register') {
-	// 	$('.nav li a:contains("Register")').addClass('active');
-	// }
-	// else if (path == '/login') {
-	// 	$('.nav li a:contains("Login")').addClass('active');
-	// }
+	var path = Router._current.path;
+	if (path == '/register') {
+		$('.nav li a:contains("Register")').addClass('active');
+	}
+	else if (path == '/login') {
+		$('.nav li a:contains("Login")').addClass('active');
+	}
+	else if (path == '/about') {
+		$('.nav li a:contains("About")').addClass('active');
+	}
+	else if (path == '/contact') {
+		$('.nav li a:contains("Contact")').addClass('active');
+	}
+	else {
+		$('.nav li a:contains("Home")').addClass('active');
+	}
 }
