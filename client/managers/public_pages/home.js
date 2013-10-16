8c0642e95080b8dc81e7020f18b87b015e43e5a7
Tim Whitaker <tmwhtkr@gmail.com>
2013-10-16 12:31:46 -0700

    restyled register page. can now create an account. need to redirect after creation and set up error messages.

--- client/managers/public_pages/home.js
+++ client/managers/public_pages/home.js
@@ -1,20 +1,23 @@
 Template.home.events({
 	'click .student-signup': function() {
-		Session.set('isStudent', true);
-		Session.set('isSchool', false);
-		Session.set('isEmployer', false);
-		Session.set('isCompany', false);
+		// Session.set('isStudent', true);
+		// Session.set('isSchool', false);
+		// Session.set('isEmployer', false);
+		// Session.set('isCompany', false);
+		Session.set('registerType', 'Student');
 	},
 	'click .company-signup': function() {
-		Session.set('isStudent', false);
-		Session.set('isSchool', false);
-		Session.set('isEmployer', true);
-		Session.set('isCompany', false);
+		// Session.set('isStudent', false);
+		// Session.set('isSchool', false);
+		// Session.set('isEmployer', true);
+		// Session.set('isCompany', false);
+		Session.set('registerType', 'Employer');
 	},
 	'click .school-signup': function() {
-		Session.set('isStudent', false);
-		Session.set('isSchool', true);
-		Session.set('isEmployer', false);
-		Session.set('isCompany', false);
+		// Session.set('isStudent', false);
+		// Session.set('isSchool', true);
+		// Session.set('isEmployer', false);
+		// Session.set('isCompany', false);
+		Session.set('registerType', 'Faculty');
 	},
 });
--- client/managers/public_pages/register.js
+++ client/managers/public_pages/register.js
@@ -10,8 +10,7 @@ Template.register.helpers({
 	},
 	isCompany: function() {
 		return Session.get('isCompany');
-	}
-
+	},
 });
 
 Template.register.events({
@@ -67,5 +66,82 @@ Template.register.events({
 		// 	console.log('no match');
 		// }
 		// console.log(password, e.target.value);
+	}
+});
+
+Template.register.rendered = function() {
+	$('#radio').buttonset();
+	var type = Session.get('registerType');
+	if (type == 'Student') {
+		$('#radio1').click();
+	}
+	else if (type == 'Employer') {
+		$('#radio2').click();
+	}
+	else if (type == 'Faculty') {
+		$('#radio3').click();
+	}
+	else {
+		$('#radio1').click();
+	}
+}
+
+Template.registerInfo.helpers({
+	registerTitle: function() {
+		return Session.get('registerType');
+	},
+	registerImage: function() {
+		var type = Session.get('registerType');
+		if (type == 'Student') {
+			return '/images/student.jpg';
+		}
+		else if (type == 'Employer') {
+			return '/images/company.jpg';
+		}
+		else if (type == 'Faculty') {
+			return '/images/school.jpg';
+		}
+	}
+});
+
+Template.registerForm.events({
+	'click #radio': function() {
+		var type = $('#radio :radio:checked + label').text();
+		Session.set('registerType', type);
+	},
+	'click .submit': function() {
+		var fname = $('#fname').val();
+		var lname = $('#lname').val();
+		var email = $('#email').val();
+		var password = $('#password').val();
+		var confirm = $('#confirm').val();
+		var emailRegEx = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+		var type = Session.get('registerType');
+
+		if (!email.match(emailRegEx)) {
+			//throwError('Email is invalid.');
+		}
+		else if (password != confirm) {
+			//throwError('Passwords don't match.');
+		}
+		else {
+			Accounts.createUser({
+				email: email,
+				password: password,
+				profile: {
+					fname: fname,
+					lname: lname,
+					type: type
+				}
+			}, function (error) {
+				if (error) {
+					//throwError('Error creating account.');
+				}
+				else {
+					console.log('Success');
+					Meteor.go('home');
+				}
+			});
+		}
 	}
 });
